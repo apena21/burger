ad30ce171247c029c7ed6d367fe383227ff4b83d
Anna P <anna_p@mail.com>
2018-03-19 21:51:00 -0400

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burger (burger_name) VALUES ('Veggie Deluxe');
+INSERT INTO burger (burger_name) VALUES ('Chicken Burger');
+INSERT INTO cats (burger_name, devoured) VALUES ('Cheese Burger', true);
+
