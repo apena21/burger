f2619465979a5761a74865d382c9b627a7c014eb
Anna P <anna_p@mail.com>
2018-03-23 22:08:22 -0400

    changes to html and css

--- models/burger.js
+++ models/burger.js
@@ -3,18 +3,18 @@ const orm = require("../config/orm.js");
 
 const burger = {    
     all: function(cb) {
-      orm.all("burgers", function(res) {
+      orm.all("burger", function(res) {
         cb(res);
       });
     },
     // The variables cols and vals are arrays.
     create: function(cols, vals, cb) {
-      orm.create("burgers", cols, vals, function(res) {
+      orm.create("burger", cols, vals, function(res) {
         cb(res);
       });
     },
     update: function(objColVals, condition, cb) {
-      orm.update("burgers", objColVals, condition, function(res) {
+      orm.update("burger", objColVals, condition, function(res) {
         cb(res);
       });
     },
